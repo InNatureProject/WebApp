a3eeced513c74ffe11b0b592e0282f96ab7899fd
InNatureProject <141932552+InNatureProject@users.noreply.github.com>
2023-08-11 22:11:49 +0000

    3

--- app.js
+++ app.js
@@ -3,24 +3,21 @@ const cors                  = require("cors");
 const db                    = require("./query");
 const app = new express();
 
-const imgs = ["https://fenixculatra.github.io/PlantasMedicinais/imagens/capim-limao.jpg", ]
+const imgs = ["https://fenixculatra.github.io/PlantasMedicinais/imagens/capim-limao.jpg", "https://fenixculatra.github.io/PlantasMedicinais/imagens/hortela.jpg", "https://fenixculatra.github.io/PlantasMedicinais/imagens/camomila.jpg"]
 
 app.set("view engine", "ejs");
 app.set("views", "./pages");
 
 app.use(cors());
+app.use(express.static(__dirname + "/public"))
 
 //Generate
 app.get("/", (req, res) => {
-    text = `
-    <a href="atividade1">Atividade 1</a>
-    `;
-    res.send(text);
+    res.render("index", {imgs:imgs});
 })
 
 app.get("/plantas", async (req, res) => {
         let j = await db.getAllPlantas(5);
-
         console.log(j);
         res.send(j);
     })
