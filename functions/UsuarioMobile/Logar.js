561248d25a681b120db87fd94671203f27b715ed
InNatureProject <141932552+InNatureProject@users.noreply.github.com>
2023-10-24 11:43:03 +0000

    1.8.2

---
+++ functions/UsuarioMobile/Logar.js
@@ -0,0 +1,45 @@
+// const { response } = require("express");
+const User    = require("../Banco/User");
+const Token   = require("jsonwebtoken");
+const res     = require("express/lib/response");
+const Validar = require("./Validar");
+
+const Logar = async (user, res) => {
+    let email = user.email;
+    let senha = user.senha;
+
+
+    if (!email || !senha) {
+        return {erro: "Dados Insuficientes"};
+    }
+
+
+
+    if (!(Validar.validarEmail(email) && Validar.validarSenha(senha))) {
+        return {erro: "Dados Inválidos"};
+    }
+
+
+    let Find = await User.find(email, senha)
+    .then(response => {
+        return response;
+        
+    }).catch(erro => {
+        return {erro: erro};
+    })
+
+    if (Find.length < 1 || Find.erro) {
+        return {erro: 'Informações Incorretas'};
+    }
+
+    let token = await Token.sign({
+        id: Find[0].cod_usr,
+        nome: Find[0].nome,
+        email: Find[0].email,
+    }, "Plantas2354Senha");
+
+    res.cookie("Token", token);
+    // res.redirect("usuario");
+    res.sendStatus(200);
+}
+module.exports = Logar;
